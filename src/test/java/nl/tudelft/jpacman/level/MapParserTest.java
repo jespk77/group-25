c6ce268513f3e6e3cbacc1a0c92853813c0fa1f4
Gerlof Fokkema <gerlof.fokkema@gmail.com>
2014-05-25 21:34:37 +0200

    Part II, Mocks finished. Fixed CheckStyle violations and updated report

--- src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
+++ src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
@@ -25,23 +25,28 @@ import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+/**
+ * This class tests the functionality of the JPacman MapParser.
+ * @author gerlof
+ */
 @RunWith(MockitoJUnitRunner.class)
 public class MapParserTest {
 	private MapParser parser;
 	private List<String> grid1;
 	private List<String> grid2;
 	
-	@Mock LevelFactory levelCreator;
-	@Mock BoardFactory boardCreator;
-	@Mock Ghost ghost;
-	@Mock Pellet pellet;
-	@Mock Square square;
-	@Mock Level level;
+	@Mock private LevelFactory levelCreator;
+	@Mock private BoardFactory boardCreator;
+	@Mock private Ghost ghost;
+	@Mock private Pellet pellet;
+	@Mock private Square square;
 	
-	@Captor ArgumentCaptor<List<Square>> captor;
-	
-	private List<Square> squares = new ArrayList<Square>();
+	@Captor private ArgumentCaptor<List<Square>> captor;
 
+	/**
+	 * Sets up the MapParser using a simple map.
+	 * Also initializes all needed mock methods.
+	 */
 	@Before
 	public void setUp() {
 		grid1 = new ArrayList<String>();
@@ -51,7 +56,6 @@ public class MapParserTest {
 		grid2 = new ArrayList<String>();
 		
 		parser = new MapParser(levelCreator, boardCreator);
-		squares.add(square);
 		
 		when(levelCreator.createGhost()).thenReturn(ghost);
 		when(levelCreator.createPellet()).thenReturn(pellet);
@@ -59,37 +63,64 @@ public class MapParserTest {
 		when(boardCreator.createWall()).thenReturn(square);
 	}
 	
+	/**
+	 * Given the map grid1, the MapParser should
+	 * create exactly 5 ground tiles.
+	 */
 	@Test
 	public void ground() {		
 		parser.parseMap(grid1);
 		verify(boardCreator, times(5)).createGround();
 	}
 	
+	/**
+	 * Given the map grid1, the MapParser should
+	 * create exactly 1 Ghost NPC.
+	 */
 	@Test
 	public void ghost() {
 		parser.parseMap(grid1);
 		verify(levelCreator, times(1)).createGhost();
 	}
 	
+	/**
+	 * Given the map grid1, the MapParser should
+	 * create exactly 1 Pellet.
+	 */
 	@Test
 	public void pellet() {
 		parser.parseMap(grid1);
 		verify(levelCreator, times(1)).createPellet();
 	}
 	
+	/**
+	 * Given the map grid1, the MapParser should
+	 * create exactly a List of startingSquares with size 1.
+	 */
 	@Test
 	public void player() {
+		Board board = any(Board.class);
+		List<NPC> ghosts = anyListOf(NPC.class);
+		
 		parser.parseMap(grid1);
-		verify(levelCreator, times(1)).createLevel(any(Board.class), anyListOf(NPC.class), captor.capture());
+		verify(levelCreator, times(1)).createLevel(board, ghosts, captor.capture());
 		assertEquals(1, captor.getValue().size());
 	}
 	
+	/**
+	 * Given the map grid1, the MapParser should
+	 * create exactly 1 wall tile.
+	 */
 	@Test
 	public void wall() {
 		parser.parseMap(grid1);
 		verify(boardCreator, times(1)).createWall();
 	}
 	
+	/**
+	 * Given the map grid2, which we initialize with invalid values,
+	 * the MapParser should throw a {@link PacmanConfigurationException}.
+	 */
 	@Test(expected = PacmanConfigurationException.class)
 	public void invalidChar() {
 		grid2.add("C ");
@@ -97,6 +128,10 @@ public class MapParserTest {
 		parser.parseMap(grid2);
 	}
 	
+	/**
+	 * Given the map grid2, which we initialize with different row lengths,
+	 * the MapParser should throw a {@link PacmanConfigurationException}.
+	 */
 	@Test(expected = PacmanConfigurationException.class)
 	public void invalidSize() {
 		grid2.add("  ");
@@ -104,12 +139,20 @@ public class MapParserTest {
 		parser.parseMap(grid2);
 	}
 	
+	/**
+	 * Given the map grid2, which we initialize with an empty String,
+	 * the MapParser should throw a {@link PacmanConfigurationException}.
+	 */
 	@Test(expected = PacmanConfigurationException.class)
 	public void invalidString() {
 		grid2.add("");
 		parser.parseMap(grid2);
 	}
 	
+	/**
+	 * Given the map grid2, which we do not initialize,
+	 * the MapParser should throw a {@link PacmanConfigurationException}.
+	 */
 	@Test(expected = PacmanConfigurationException.class)
 	public void invalidNull() {
 		parser.parseMap(grid2);
