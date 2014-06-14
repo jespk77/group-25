bb58bb5ab302d6b1fda68f9eb8bca47c30b023b6
Gerlof Fokkema <gerlof.fokkema@gmail.com>
2014-06-14 18:39:45 +0200

    Started the first test case, and the report

---
+++ src/test/java/nl/tudelft/jpacman/game/Game.java
@@ -0,0 +1,14 @@
+package nl.tudelft.jpacman.game;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Game {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
